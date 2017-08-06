faa067498edc008bc4fb628877ba1e8e471160ab
偉欣 林 <wayne70309@gmail.com>
2017-08-06 22:12:02 +0800

    homework done

---
+++ AccountingTable/Controllers/AccountingController.cs
@@ -0,0 +1,25 @@
+﻿using AccountingTable.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AccountingTable.Controllers
+{
+    public class AccountingController : Controller
+    {
+        public ActionResult Accounting( )
+        {
+            var fakeDataList = new List<AccountingViewModel>
+            {
+                new AccountingViewModel{Type="支出",Date=DateTime.Parse("2017/01/01"),Amount=1000},
+                new AccountingViewModel{Type="支出",Date=DateTime.Parse("2017/01/02"),Amount=500},
+                new AccountingViewModel{Type="支出",Date=DateTime.Parse("2017/01/03"),Amount=1500},
+                new AccountingViewModel{Type="支出",Date=DateTime.Parse("2017/01/04"),Amount=20000}
+            };
+
+            return View( fakeDataList );
+        }
+    }
+}
---
+++ AccountingTable/Models/ViewModels/AccountingViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AccountingTable.Models.ViewModels
+{
+    public class AccountingViewModel
+    {
+        [DisplayName( "類別" )]
+        public string Type { get; set; }
+        [DisplayName( "日期" )]
+        [DataType( DataType.Date )]
+        [DisplayFormat( DataFormatString = "{0:yyyy-MM-dd}" )]
+        public DateTime Date { get; set; }
+        [DisplayName( "金額" )]
+        [DisplayFormat( DataFormatString = "{0:#,0}" )]
+        public decimal Amount { get; set; }
+    }
+}
